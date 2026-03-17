c9a8aa917364cdbb1da226f7339d1b280ab43666
dakenmi <xiaobedoty@gmail.com>
2026-03-17 18:36:34 +0300

    added the game brain_gcd

---
+++ scripts/brain_gcd.py
@@ -0,0 +1,35 @@
+from random import randint
+from brain_games.cli import welcome_user
+
+
+name = welcome_user()
+
+
+def gcd_check():
+    print(f'Find the greatest common divisor of given numbers.')
+    counter = 0
+    while counter < 3:
+        numbers = [randint(1,1000), randint(1,1000)]
+        if numbers[0] > numbers[1]:
+            for i in range(numbers[1], 0):
+                if numbers[0] % i == 0 and numbers[1] % i == 0:
+                    right_answer = i
+        elif numbers[1] > numbers[0]:
+            for i in range(numbers[0], 0):
+                if numbers[1] % i == 0 and numbers[0] % i == 0:
+                    right_answer = i
+        else:
+            right_answer = numbers[0]
+        print(f'Question: {numbers[0]} {numbers[1]}')
+        user_answer = int(input())
+        if user_answer == right_answer:
+            if counter < 2:
+                print('Correct!')
+                counter += 1
+            else:
+                print(f'Congratulations, {name}!')
+                counter += 1
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{right_answer}'.")
+            print(f"Let's try again, {name}!")
+            break
