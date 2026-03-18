faffae526926f63a36228d8deea580262e3894b2
dakenmi <xiaobedoty@gmail.com>
2026-03-18 21:16:01 +0300

    test fo game brain_prime

---
+++ scripts/brain_prime.py
@@ -0,0 +1,24 @@
+from random import randint
+from brain_games.cli import welcome_user
+
+
+name = welcome_user()
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def prime_check():
+    print('Answer "yes" if given number is prime. Otherwise answer "no"')
+    while True:
+        number = randint(1, 1000)
+        print(f'Question: {number}')
+        user_answer = input()
+        right_answer = 'yes' if is_prime(number) else 'no'
+        if user_answer == right_answer:
+            print('Correct!')
